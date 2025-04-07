92b4e7a9d753a23a860cf8dfdc9f7f6eea118deb
Francine Gonçalves Nunes Moraes <francinegnmoraes@gmail.com>
2025-04-07 18:13:37 -0300

    logical changes of dark mode in js

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ function toggleMode() {
 
     const img = document.querySelector("#profile img")
 
-    if (html.classList.contains('dark')) {
+    const isDarkMode = html.classList.contains('dark')
+
+    if (isDarkMode) {
         img.setAttribute('src', './assets/avatar.png')
         img.setAttribute('alt', "Foto de menina de cabelo tom marsala, médio e liso com leve maquiagem marsala e delineado preto. Usa um colar dourado e vestido em tom preto e esboça um leve sorriso. Em fundo rosa pastel")
     } else {
